4a01ce3beffd3c54dea517a3807a04ffc600068d
alessiasabia <a.sabia15@studenti.unisa.it>
2020-12-30 13:18:10 +0100

    creazione StudenteBean

---
+++ src/main/java/com/uniseats/prenotazioni/StudenteBean.java
@@ -0,0 +1,65 @@
+package com.uniseats.prenotazioni;
+
+public class StudenteBean {
+
+    private String nome;
+    private String cognome;
+    private int matricola;
+    private String email;
+    private String password;
+    private int anno;
+    private String dipartimento;
+
+    private ArrayList<PrenotazioneBean> prenotazione;
+
+    public StudenteBean() {
+
+    }
+
+    public StudenteBean(String nome, String cognome, int matricola, String email, String password, int anno, String dipartimento) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.matricola = matricola;
+        this.email = email;
+        this.password = password;
+        this.anno = anno;
+        this.dipartimento = dipartimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public String getDipartimento() {
+        return dipartimento;
+    }
+
+    public ArrayList<PrenotazioneBean> getPrenotazione() {
+        return prenotazione;
+    }
+}
