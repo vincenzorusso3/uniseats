fadbf2b497566dd134c0fe7c80fdeeee0e510e79
Simone <s.silvestri15@studenti.unisa.it>
2020-12-31 13:05:18 +0100

    Added dataSources utils

---
+++ src/main/java/it/uniseats/Utils/DataSourceUtils.java
@@ -0,0 +1,27 @@
+package it.uniseats.Utils;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DataSourceUtils {
+
+    public static DataSource getDataSource() {
+
+        DataSource ds = null;
+
+        try {
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+            ds = (DataSource) envCtx.lookup("jdbc/uniseats");
+
+        } catch (NamingException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+
+        return ds;
+    }
+
+}
