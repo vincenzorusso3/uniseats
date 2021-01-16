7041bd780d48fc826f86546301811a4a994cb284
benedettosimone <sim.benede@gmail.com>
2021-01-16 11:40:33 +0100

    Javadoc su Aula e Posto Bean

--- src/main/java/it/uniseats/model/beans/AulaBean.java
+++ src/main/java/it/uniseats/model/beans/AulaBean.java
@@ -4,12 +4,27 @@ import java.util.List;
 
 public class AulaBean {
 
+    /**
+     * Variabili d'istanza
+     */
     private String codice;
     private String dipartimento;
     private int nPosti;
     private String edificio;
 
+    /**
+     * Costruttore vuoto
+     */
+
     public AulaBean() {}
+
+    /**
+     * Costruttore
+     * @param codice rappresenta il codice identificativo di un'aula
+     * @param dipartimento rappresenta a quale dipartimento appartiene
+     * @param nPosti rappresenta quanti posti contiene
+     * @param edificio rappresenta in quale edificio è collocata
+     */
 
     public AulaBean(String codice, String dipartimento, int nPosti, String edificio) {
         this.codice = codice;
@@ -18,28 +33,62 @@ public class AulaBean {
         this.edificio = edificio;
     }
 
+
+    /**
+     *
+     * @return codice aula
+     */
     public String getCodice() {
         return codice;
     }
 
+    /**
+     *
+     * @return dipartimento
+     */
     public String getDipartimento() {
         return dipartimento;
     }
 
+    /**
+     *
+     * @return numero posti
+     */
     public int getnPosti() {
         return nPosti;
     }
 
+
+    /**
+     *
+     * @return edificio
+     */
     public String getEdificio() {
         return edificio;
     }
 
+    /**
+     * Modifica il codice dell'aula
+     * @param codice identificativo di un'aula
+     */
     public void setCodice(String codice) { this.codice = codice; }
 
+    /**
+     * Modifica il dipartimento di un'aula
+     * @param dipartimento è il dipartimento di appartenenza
+     */
     public void setDipartimento(String dipartimento) { this.dipartimento = dipartimento; }
 
+    /**
+     * Modifica il numero di posti di un'aula
+     * @param nPosti posti totali di un'aula
+     */
     public void setnPosti(int nPosti) { this.nPosti = nPosti; }
 
+    /**
+     * Modifica l'edificio di un'aula
+     * @param edificio edificio in cui è collocata
+     */
     public void setEdificio(String edificio) { this.edificio = edificio; }
 
 
--- src/main/java/it/uniseats/model/beans/PostoBean.java
+++ src/main/java/it/uniseats/model/beans/PostoBean.java
@@ -2,10 +2,20 @@ package it.uniseats.model.beans;
 
 public class PostoBean {
 
+    /**
+     * Variabile istanza
+     */
     private String codice;
 
+    /**
+     * Costruttore vuoto
+     */
     public PostoBean() { }
 
+    /**
+     * Costruttore
+     * @param codice codice identificativo di un posto
+     */
     public PostoBean(String codice) {
         this.codice = codice;
     }
