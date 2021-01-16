e53a5126172e29df82879855136db8d6a7c1b549
matthew-2000 <m.ercolino1@studenti.unisa.it>
2021-01-16 12:33:59 +0100

    add PostoBeanTest and fix PostoBean constructor

---
+++ src/test/java/PostoBeanTest.java
@@ -0,0 +1,60 @@
+import it.uniseats.model.beans.AulaBean;
+import it.uniseats.model.beans.PostoBean;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PostoBeanTest {
+
+    /**
+     * Constructors Testing
+     */
+
+    @Test
+    void testEmptyPostoConstructor() {
+        PostoBean posto = new PostoBean();
+        assertNotNull(posto);
+    }
+
+    @Test
+    void testPostoConstructor() {
+        PostoBean posto = new PostoBean("A1-01", "A1");
+        assertNotNull(posto);
+    }
+
+    /**
+     * Getter Methods Testing
+     */
+
+    @Test
+    void testGetCodice() {
+        PostoBean posto = new PostoBean("A1-01", "A1");
+        assertEquals("A1-01", posto.getCodice());
+    }
+
+    @Test
+    void testGetCodiceAula() {
+        PostoBean posto = new PostoBean("A1-01", "A1");
+        assertEquals("A1", posto.getCodiceAula());
+    }
+
+    /**
+     * Setter Metods Testing
+     */
+
+    @Test
+    void testSetCodice() {
+        PostoBean posto = new PostoBean();
+        posto.setCodice("A1-01");
+        assertEquals("A1-01", posto.getCodice());
+    }
+
+    @Test
+    void testSetCodiceAula() {
+        PostoBean posto = new PostoBean();
+        posto.setCodiceAula("A1");
+        assertEquals("A1", posto.getCodiceAula());
+    }
+
+}
