3a774eeb206e69fbea90b84b38b18935b47b560a
alessiasabia <a.sabia15@studenti.unisa.it>
2021-01-19 16:16:56 +0100

    update LoginServletTest

--- src/test/java/LoginServletTest.java
+++ src/test/java/LoginServletTest.java
@@ -1,41 +1,49 @@
-import it.uniseats.control.gestione_accesso.LoginServlet;
-import it.uniseats.model.beans.StudenteBean;
-import it.uniseats.utils.SHA512Utils;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.mockito.Mockito;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+
+
 class LoginServletTest extends Mockito {
+    private LoginServlet servlet;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
 
-    private static LoginServlet servlet;
-    private static MockHttpServletRequest request;
-    private static MockHttpServletResponse response;
-    private static StudenteBean bean;
-
-
-    @BeforeEach
-    void load() {
+    /**
+     * Before.
+     */
+    @Before
+    public void setUp() {
         servlet = new LoginServlet();
         request = new MockHttpServletRequest();
         response = new MockHttpServletResponse();
     }
 
-   @Test
-    public void logInTest(){
-            bean = new StudenteBean("Mario", "Rossi", "0512102938", "m.rossi@studenti.unisa.it", "marioRossi",2,"Matematica");
-            request.setParameter("action","Login");
-            request.setParameter("email","a.sabia15@studenti.unisa.it");
-            request.setParameter("password", SHA512Utils.getSHA512("accioLaurea"));
-
-
-
-
-        }
-
+    @Test
+    public void logInTest() throws ServletException, IOException  {
+        request.setParameter("email", "a.sabia15@studenti.unisa.it");
+        request.setParameter("password", "accioLaurea");
+        assertThrows(IllegalArgumentException.class, () -> {
+            servlet.doGet(request, response);
+        });
+    }
 
 
 
 }
+
+
+
+
+
