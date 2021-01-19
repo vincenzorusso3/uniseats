86b20ff955f34b0a1175904b7bd014061b582080
matthew-2000 <m.ercolino1@studenti.unisa.it>
2021-01-19 12:22:10 +0100

    StudenteDAO testing

--- src/test/java/StudenteDAOTest.java
+++ src/test/java/StudenteDAOTest.java
@@ -3,11 +3,10 @@ import it.uniseats.model.dao.StudenteDAO;
 import it.uniseats.utils.SHA512Utils;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.sql.SQLException;
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StudenteDAOTest {
 
@@ -26,7 +25,26 @@ public class StudenteDAOTest {
         assertNotNull(studenti);
     }
 
+    @Test
+    public void doRetrieveByEmailTest() throws SQLException {
+        StudenteBean bean = (StudenteBean) StudenteDAO.doQuery("doRetrieveByEmail","m.ercolino1@studenti.unisa.it");
+        StudenteBean expected = new StudenteBean("Matteo", "Ercolino", "0512105933", "m.ercolino1@studenti.unisa.it", "AlessiaLove3000", 3, "Informatica");
+        assertTrue(expected.getMatricola().equals(bean.getMatricola()) && expected.getNome().equals(bean.getNome()) &&
+                expected.getCognome().equals(bean.getCognome()) && expected.getEmail().equals(bean.getEmail()) && SHA512Utils.getSHA512(expected.getPassword()).equals(bean.getPassword()) &&
+                expected.getAnno() == bean.getAnno() && expected.getDipartimento().equals(bean.getDipartimento()));
+    }
 
+    @Test
+    public void doRetrieveByMatricolaFailTest() throws SQLException {
+        StudenteBean bean = (StudenteBean) StudenteDAO.doQuery("doRetrieveByMatricola","hdhjs");
+        assertNull(bean.getMatricola());
+    }
+
+    @Test
+    public void doRetrieveByEmailFailTest() throws SQLException {
+        StudenteBean bean = (StudenteBean) StudenteDAO.doQuery("doRetrieveByEmail","hdhjs");
+        assertNull(bean.getEmail());
+    }
 
 }
 
