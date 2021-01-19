e104a172575c080951925bbee34e46e774bc271f
alessiasabia <a.sabia15@studenti.unisa.it>
2021-01-19 13:52:49 +0100

    added Spring Frameworks

---
+++ src/test/java/LoginServletTest.java
@@ -0,0 +1,13 @@
+import it.uniseats.control.gestione_accesso.LoginServlet;
+import org.mockito.Mockito;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class LoginServletTest extends Mockito {
+
+    private LoginServlet servlet;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+
+}
