44d096bda34a0c5fd7da1754e266272ad76bc907
matthew-2000 <m.ercolino1@studenti.unisa.it>
2021-01-20 21:41:31 +0100

    fix checkstyle 2.0

--- src/test/java/RegistrazioneServletTest.java
+++ src/test/java/RegistrazioneServletTest.java
@@ -1,4 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import it.uniseats.control.gestione_utente.RegistrazioneServlet;
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -6,47 +11,41 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.mock.web.MockServletConfig;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import java.io.IOException;
+class RegistrazioneServletTest {
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+  private RegistrazioneServlet servlet;
+  private MockHttpServletRequest request;
+  private MockHttpServletResponse response;
+  private MockHttpSession session;
 
-public class RegistrazioneServletTest {
+  @BeforeEach
+  void setUp() {
+    servlet = new RegistrazioneServlet();
+    request = new MockHttpServletRequest();
+    response = new MockHttpServletResponse();
+    request.setSession(session);
 
-    private RegistrazioneServlet servlet;
-    private MockHttpServletRequest request;
-    private MockHttpServletResponse response;
-    private MockHttpSession session;
+  }
 
-    @BeforeEach
-    public void setUp() {
-        servlet = new RegistrazioneServlet();
-        request = new MockHttpServletRequest();
-        response = new MockHttpServletResponse();
-        request.setSession(session);
+  @BeforeEach
+  void oneWaySetup() throws ServletException {
+    ServletConfig sg = new MockServletConfig();
+    servlet.init(sg);
+  }
 
-    }
+  @Test
+  void registrazioneTest() throws ServletException, IOException {
+    request.addParameter("action", "add");
 
-    @BeforeEach
-    public void oneWaySetup() throws ServletException {
-        ServletConfig sg = new MockServletConfig();
-        servlet.init(sg);
-    }
-
-    @Test
-    public void registrazioneTest() throws ServletException, IOException {
-        request.addParameter("action", "add");
-
-        request.addParameter("email", "");
-        request.addParameter("password", "Antonio1234-");
-        request.addParameter("matricola", "0512102852");
-        request.addParameter("anno","1");
-        request.addParameter("dipartimento", "Informatica");
-        request.addParameter("nome", "Antonio");
-        request.addParameter("cognome", "Allidato");
-        servlet.doPost(request, response);
-        assertEquals("Registrazione ", request.getAttribute("message"));
-    }
+    request.addParameter("email", "");
+    request.addParameter("password", "Antonio1234-");
+    request.addParameter("matricola", "0512102852");
+    request.addParameter("anno", "1");
+    request.addParameter("dipartimento", "Informatica");
+    request.addParameter("nome", "Antonio");
+    request.addParameter("cognome", "Allidato");
+    servlet.doPost(request, response);
+    assertEquals("Registrazione ", request.getAttribute("message"));
+  }
 
 }
