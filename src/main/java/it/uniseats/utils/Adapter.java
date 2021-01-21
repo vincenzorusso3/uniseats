a63010d5e78bb911857e0458baffd35f70289bf8
matthew-2000 <m.ercolino1@studenti.unisa.it>
2021-01-21 15:31:50 +0100

    finita prenotazione giorno corrente

--- src/main/java/it/uniseats/utils/Adapter.java
+++ src/main/java/it/uniseats/utils/Adapter.java
@@ -94,36 +94,23 @@ public class Adapter {
             ArrayList<PostoBean> posti =
                 (ArrayList<PostoBean>) PostoDAO.doQuery(PostoDAO.doRetrieveByAulaCode, aula.getCodice());
 
-            for (PostoBean posto : posti) {
-              System.out.println(posto.getCodice());
+            if (posti != null) {
 
-              if (prenotazioniList.isEmpty()) {
-                System.out.println("LISTA VUOTA");
+              for (PrenotazioneBean pren : prenotazioniList) {
+
+                posti.removeIf(posto -> posto.getCodice().equals(pren.getCodicePosto()));
+
+              }
+
+              if (!posti.isEmpty()) {
+
+                p.setCodicePosto(posti.get(0).getCodice());
                 p.setCodiceAula(aula.getCodice());
-                p.setCodicePosto(posto.getCodice());
                 break;
-              }
-
-              for (PrenotazioneBean prenotazione : prenotazioniList) {
-                System.out.println(prenotazione.getCodice());
-
-                if (!posto.getCodice().equals(prenotazione.getCodicePosto())) {
-
-                  p.setCodiceAula(aula.getCodice());
-                  p.setCodicePosto(posto.getCodice());
-                  break;
-
-                }
 
               }
-
-              if (!p.getCodicePosto().equals("00"))
-                break;
 
             }
-
-            if (!p.getCodicePosto().equals("00"))
-              break;
 
           }
 
