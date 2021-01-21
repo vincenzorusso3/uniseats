fb49826caae70fe0d0aef674ac546a8b507b1c42
benedettosimone <sim.benede@gmail.com>
2021-01-21 15:03:41 +0100

    update javadoc

--- src/main/java/it/uniseats/utils/DateUtils.java
+++ src/main/java/it/uniseats/utils/DateUtils.java
@@ -5,19 +5,39 @@ import java.text.ParseException;
 import java.util.Date;
 import java.util.Locale;
 
+/**
+ * Un oggetto DateUtils serve per convertire una data in vari formati
+ */
+
 public class DateUtils {
 
   private static final DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.ITALY);
 
+  /**
+   * Metodo per trasfomare una stringa in Date
+   * @param date una <b>data</b> in formato stringa
+   * @return <b>data</b>trasformata
+   * @throws ParseException
+   */
   public static Date parseDate(String date) throws ParseException {
     date = date.replace("-", "/");
     return df.parse(date);
   }
 
+  /**
+   * Metodo per trasformare la data in una stringa
+   * @param date <b>data</b>
+   * @return <b>data</b> in stringa
+   */
   public static String dateToString(Date date) {
     return df.format(date).replace("-", "/");
   }
 
+  /**
+   * Metodo per trasformare una stringa (data con formato inglese) in stringa (data con formato italiano)
+   * @param s <b>stringa</b> da convertire
+   * @return <b>stringa</b> convertita
+   */
   public static String englishToItalian(String s) {
 
     String year = (String) s.subSequence(0, 4);
