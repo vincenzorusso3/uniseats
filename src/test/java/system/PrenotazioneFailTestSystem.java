7dccc424e4fd8dbbcc1eee03ddd1ab0400ee12d9
benedettosimone <sim.benede@gmail.com>
2021-01-26 18:30:06 +0100

    added prenotazionefailTestSystem

---
+++ src/test/java/system/PrenotazioneFailTestSystem.java
@@ -0,0 +1,77 @@
+package system;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class PrenotazioneFailTestSystem {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testPrenotazione() throws Exception {
+    driver.get("http://localhost:8080/UniSeats_war_exploded/view/LandingPageView.jsp");
+    driver.findElement(By.xpath("//div[3]/div[2]")).click();
+    driver.findElement(By.xpath("(//a[contains(text(),'Prenota')])[3]")).click();
+    driver.findElement(By.id("calendar")).click();
+    driver.findElement(By.id("calendar")).clear();
+    driver.findElement(By.id("calendar")).sendKeys("2021-01-27");
+    driver.findElement(By.id("btnPrenotazioneSingola")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
