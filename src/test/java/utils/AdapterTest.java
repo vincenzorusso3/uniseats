982eb49089237aaccdf5bac4832d07b8fe500c76
matthew-2000 <m.ercolino1@studenti.unisa.it>
2021-01-29 17:48:40 +0100

    75% (first reaction: Shock!)

--- src/test/java/utils/AdapterTest.java
+++ src/test/java/utils/AdapterTest.java
@@ -11,6 +11,7 @@ import it.uniseats.utils.Adapter;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.util.Date;
 import java.util.Locale;
 import org.junit.jupiter.api.Test;
 
@@ -28,6 +29,27 @@ class AdapterTest {
     PrenotazioneBean prenotazione =
         new PrenotazioneBean("1-11111111-111111", df.parse("25/12/2021"), true, "00", "00",
             "0512105933");
+    PrenotazioneDao.doQuery("doSave", prenotazione);
+
+    StudenteBean studente = new StudenteBean("TestNome", "TestCognome", "0512103231",
+        "testnome.testcognome@studenti.unisa.it", "testnomecognome", 2, "Matematica");
+    StudenteDao.doQuery("doSave", studente);
+
+    assertTrue(Adapter.listener(prenotazione, studente));
+
+    StudenteDao.doQuery("doDelete", studente.getMatricola());
+    PrenotazioneDao.doQuery("doDelete", "1-11111111-111111");
+
+  }
+
+  @Test
+  void prenotazioneGiornoCorrenteTest()
+      throws ParseException, SQLException, CloneNotSupportedException {
+
+    PrenotazioneBean prenotazione =
+        new PrenotazioneBean("1-11111111-111111", new Date(), true, "00", "00",
+            "0512105933");
+
     PrenotazioneDao.doQuery("doSave", prenotazione);
 
     StudenteBean studente = new StudenteBean("TestNome", "TestCognome", "0512103231",
