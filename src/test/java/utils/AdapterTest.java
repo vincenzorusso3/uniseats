41cefc6d2462b533324269fbf57ee85b24f1e29e
DanieleSalerno <d.salerno8@studenti.unisa.it>
2021-02-04 12:29:46 +0100

    Added PrenotarePosto TC_1.4_*

--- src/test/java/utils/AdapterTest.java
+++ src/test/java/utils/AdapterTest.java
@@ -1,5 +1,6 @@
 package utils;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import it.uniseats.model.beans.PrenotazioneBean;
